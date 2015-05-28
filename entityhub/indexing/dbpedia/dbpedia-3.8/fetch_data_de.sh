3c509e4425390dc73c8fa9719a9fc825a3e0671b
tastatur <repkov.denis@gmail.com>
2015-05-28 11:07:04 +0200

    DBpedia 3.9

--- entityhub/indexing/dbpedia/dbpedia-3.8/fetch_data_de.sh
+++ entityhub/indexing/dbpedia/dbpedia-3.8/fetch_data_de.sh
@@ -18,7 +18,7 @@
 
 INDEXING_JAR=`pwd`/target/org.apache.stanbol.entityhub.indexing.dbpedia-0.12.1-SNAPSHOT.jar
 WORKSPACE=~/tmp/dbpedia-index
-DBPEDIA=http://downloads.dbpedia.org/3.8
+DBPEDIA=http://downloads.dbpedia.org/3.9
 MAX_SORT_MEM=2G
 
 # Turn on echoing and exit on error

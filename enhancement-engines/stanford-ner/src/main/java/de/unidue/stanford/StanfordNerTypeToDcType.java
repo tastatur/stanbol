cf491acfdfa1f1af24596c9467d139fa423e5670
tastatur <repkov.denis@gmail.com>
2015-06-04 15:26:40 +0200

    Neue Typen

--- enhancement-engines/stanford-ner/src/main/java/de/unidue/stanford/StanfordNerTypeToDcType.java
+++ enhancement-engines/stanford-ner/src/main/java/de/unidue/stanford/StanfordNerTypeToDcType.java
@@ -14,8 +14,16 @@ public class StanfordNerTypeToDcType implements Function<String, UriRef> {
             return OntologicalClasses.DBPEDIA_PLACE;
         } else if (nerType.contains("ORG")) {
             return OntologicalClasses.DBPEDIA_ORGANISATION;
+        } else if (nerType.contains("MONEY")) {
+            return new UriRef("http://dbpedia.org/ontology/Currency");
+        } else if (nerType.contains("TIME")) {
+            return new UriRef("http://schema.org/Time");
+        } else if (nerType.contains("DATE")) {
+            return new UriRef("http://schema.org/Date");
+        } else if (nerType.contains("NUMBER")) {
+            return new UriRef("http://schema.org/Number");
         } else {
-            return new UriRef("http://dbpedia.org/ontology/Thing");
+            return new UriRef("http://www.w3.org/2002/07/owl#Thing");
         }
     }
 }

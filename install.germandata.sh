3c00a7353826c6aa34e0007fe923f453c97213da
tastatur <repkov.denis@gmail.com>
2015-08-19 17:41:53 +0200

    MITIE wird automatisch mitinstalliert

--- install.germandata.sh
+++ install.germandata.sh
@@ -8,5 +8,7 @@ mkdir -p stanbol/datafiles/
 cp $DATADIR/dedbpedia.solrindex.zip stanbol/datafiles/
 cp $DATADIR/tiger.bin stanbol/datafiles/
 cp $DATADIR/pig.bin stanbol/datafiles/
+cp $DATADIR/tiger-mt-model.dat stanbol/datafiles/
+cp $DATADIR/pig-mt-model.dat stanbol/datafiles/
 
 echo "Um die Installation abzuschliessen, starte bitte Stanbool, dann gehe auf http://localhost:8080/system/console/bundles und installiere Bundle org.apache.stanbol.data.site.dedbpedia-1.0.0.jar, den du in $DATADIR finden solltest"

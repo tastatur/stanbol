6d1d6f66bbb5648844a0ea03d9b88ae24efd665b
tastatur <repkov.denis@gmail.com>
2015-08-23 01:27:01 +0200

    Jar launcher muss den Zugriff auf das Property stanbol.basedir auch haben

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 export LD_LIBRARY_PATH=$HOME/lib/
 if [[ "$1" == "-d" ]]; then
- java -Xdebug -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=n -Xmx4g -jar launchers/full/target/org.apache.stanbol.launchers.full-0.12.1-SNAPSHOT.jar
+ java -Xdebug -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=n -Xmx4g -Dstanbol.basedir=$(pwd) -jar launchers/full/target/org.apache.stanbol.launchers.full-0.12.1-SNAPSHOT.jar
 else
- java -Xmx4g -jar launchers/full/target/org.apache.stanbol.launchers.full-0.12.1-SNAPSHOT.jar
+ java -Xmx4g -Dstanbol.basedir=$(pwd) -jar launchers/full/target/org.apache.stanbol.launchers.full-0.12.1-SNAPSHOT.jar
 fi

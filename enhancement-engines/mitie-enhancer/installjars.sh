a0ee26ed150ab3c63571aadd041fb16362290fec
tastatur <repkov.denis@gmail.com>
2015-08-22 21:25:50 +0200

    Wir brauchen nur libjavamitie.so für JNI API

--- enhancement-engines/mitie-enhancer/installjars.sh
+++ enhancement-engines/mitie-enhancer/installjars.sh
@@ -4,5 +4,4 @@ mkdir lib
 cp $1/javamitie.jar lib/
 mkdir $HOME/lib
 cp $1/libjavamitie.so $HOME/lib
-cp $1/../../../libmitie.so $HOME/lib
 mvn install:install-file -Dfile=lib/javamitie.jar -DgroupId=edu.mit.mitie -DartifactId=mitie -Dversion=3.0 -Dpackaging=jar

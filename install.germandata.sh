26e4c9ad8ed2c9e8270ea456249c9b62da4b13a5
tastatur <repkov.denis@gmail.com>
2015-08-22 22:34:46 +0200

    Germanbundle kann als WAR-Datei deployt werden

--- install.germandata.sh
+++ install.germandata.sh
@@ -2,13 +2,14 @@
 
 
 DATADIR=$1
+STANBOLDIR=$2
 echo "Hallo! Ich installiere für dich den Index von de.dbpedia.org und Modellen für TIGER und PIG Engine. Die Dateien sind für git leider zu gross. :("
-mkdir -p stanbol/datafiles/
+mkdir -p $STANBOLDIR/datafiles/
 
-cp $DATADIR/dedbpedia.solrindex.zip stanbol/datafiles/
-cp $DATADIR/tiger.bin stanbol/datafiles/
-cp $DATADIR/pig.bin stanbol/datafiles/
-cp $DATADIR/tiger-mt-model.dat stanbol/datafiles/
-cp $DATADIR/pig-mt-model.dat stanbol/datafiles/
+cp $DATADIR/dedbpedia.solrindex.zip $STANBOLDIR/datafiles/
+cp $DATADIR/tiger.bin $STANBOLDIR/datafiles/
+cp $DATADIR/pig.bin $STANBOLDIR/datafiles/
+cp $DATADIR/tiger-mt-model.dat $STANBOLDIR/datafiles/
+cp $DATADIR/pig-mt-model.dat $STANBOLDIR/datafiles/
 
-echo "Um die Installation abzuschliessen, starte bitte Stanbool, dann gehe auf http://localhost:8080/system/console/bundles und installiere Bundle org.apache.stanbol.data.site.dedbpedia-1.0.0.jar, den du in $DATADIR finden solltest"
+echo "Um die Installation abzuschliessen, starte bitte Stanbool, dann gehe auf http://localhost:8080/system/console/bundles und installiere Bundle org.apache.$STANBOLDIR.data.site.dedbpedia-1.0.0.jar, den du in $DATADIR finden solltest"

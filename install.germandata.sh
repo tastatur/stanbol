7b3cb3596677970e0bd2c59975474507445601a1
tastatur <repkov.denis@gmail.com>
2015-08-23 11:44:15 +0200

    Basedir specified in web.xml

--- install.germandata.sh
+++ install.germandata.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 
-DATADIR=$1
-STANBOLDIR=$2
+INSTALLDIR=$1
+STANBOLDIR=/var/lib/stanbol-data/
 echo "Hallo! Ich installiere für dich den Index von de.dbpedia.org und Modellen für TIGER und PIG Engine. Die Dateien sind für git leider zu gross. :("
 mkdir -p $STANBOLDIR/datafiles/
 
-cp $DATADIR/dedbpedia.solrindex.zip $STANBOLDIR/datafiles/
-cp $DATADIR/tiger.bin $STANBOLDIR/datafiles/
-cp $DATADIR/pig.bin $STANBOLDIR/datafiles/
-cp $DATADIR/tiger-mt-model.dat $STANBOLDIR/datafiles/
-cp $DATADIR/pig-mt-model.dat $STANBOLDIR/datafiles/
+cp $INSTALLDIR/dedbpedia.solrindex.zip $STANBOLDIR/datafiles/
+cp $INSTALLDIR/tiger.bin $STANBOLDIR/datafiles/
+cp $INSTALLDIR/pig.bin $STANBOLDIR/datafiles/
+cp $INSTALLDIR/tiger-mt-model.dat $STANBOLDIR/datafiles/
+cp $INSTALLDIR/pig-mt-model.dat $STANBOLDIR/datafiles/
 
-echo "Um die Installation abzuschliessen, starte bitte Stanbool, dann gehe auf http://localhost:8080/system/console/bundles und installiere Bundle org.apache.$STANBOLDIR.data.site.dedbpedia-1.0.0.jar, den du in $DATADIR finden solltest"
+chown -R $TOMCATUSER:$TOMCATGROUP $STANBOLDIR
+echo "Um die Installation abzuschliessen, starte bitte Stanbool, dann gehe auf http://localhost:8080/system/console/bundles und installiere Bundle org.apache.$STANBOLDIR.data.site.dedbpedia-1.0.0.jar, den du in $INSTALLDIR finden solltest"
--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export LD_LIBRARY_PATH=$HOME/lib/
-if [[ "$1" == "-d" ]]; then
- java -Xdebug -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=n -Xmx4g -Dstanbol.basedir=$(pwd)/stanbol -jar launchers/full/target/org.apache.stanbol.launchers.full-0.12.1-SNAPSHOT.jar
-else
- java -Xmx4g -Dstanbol.basedir=$(pwd)/stanbol -jar launchers/full/target/org.apache.stanbol.launchers.full-0.12.1-SNAPSHOT.jar
-fi

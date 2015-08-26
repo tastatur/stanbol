d58872f27d3e22b484a561d60b33cb1fcc900fb8
tastatur <repkov.denis@gmail.com>
2015-08-26 21:12:11 +0200

    Stanbol kann ab jetzt sowohl als war-datei als auch als jar-datei ausgeführt werden

--- install.germandata.sh
+++ install.germandata.sh
@@ -2,8 +2,18 @@
 
 
 INSTALLDIR=$1
-STANBOLDIR=/var/lib/stanbol-data/
-TOMCATUSER=tomcat8
+
+if [[ "$2" == "" ]]; then
+  echo "Bitte, sage mir, ob du stanbol als eine WAR-Datei (-t) auf tomcat installieren, oder local(-l) starten möchtest"
+  exit -1
+fi
+
+if [[ "$2" == "-t" ]]; then
+   STANBOLDIR=/var/lib/stanbol-data/
+   TOMCATUSER=tomcat8
+elif [[ "$2" == "-l" ]]; then
+   STANBOLDIR=$(pwd)/stanbol
+fi
 echo "Hallo! Ich installiere für dich den Index von de.dbpedia.org und Modellen für TIGER und PIG Engine. Die Dateien sind für git leider zu gross. :("
 mkdir -p $STANBOLDIR/datafiles/
 
@@ -13,5 +23,7 @@ cp $INSTALLDIR/pig.bin $STANBOLDIR/datafiles/
 cp $INSTALLDIR/tiger-mt-model.dat $STANBOLDIR/datafiles/
 cp $INSTALLDIR/pig-mt-model.dat $STANBOLDIR/datafiles/
 
-chown -R $TOMCATUSER:$TOMCATGROUP $STANBOLDIR
+if [[ "$2" == "-t" ]]; then
+   chown -R $TOMCATUSER:$TOMCATGROUP $STANBOLDIR
+fi
 echo "Um die Installation abzuschliessen, starte bitte Stanbool, dann gehe auf http://localhost:8080/system/console/bundles und installiere Bundle org.apache.$STANBOLDIR.data.site.dedbpedia-1.0.0.jar, den du in $INSTALLDIR finden solltest"

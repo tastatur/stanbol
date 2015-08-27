f4b84887de08b1affd56a4581574957fb284f500
tastatur <repkov.denis@gmail.com>
2015-08-27 10:45:00 +0200

    EntityLinking soll anstatt EntityTagging verwendet weden (da kann man Verlinkungen automatisch referenzieren lassen)

--- enhancement-engines/mitie-enhancer/installjars.sh
+++ enhancement-engines/mitie-enhancer/installjars.sh
@@ -2,6 +2,5 @@
 
 mkdir lib
 cp $1/javamitie.jar lib/
-mkdir $HOME/lib
-cp $1/libjavamitie.so $HOME/lib
+sudo cp $1/libjavamitie.so /usr/lib/
 mvn install:install-file -Dfile=lib/javamitie.jar -DgroupId=edu.mit.mitie -DartifactId=mitie -Dversion=3.0 -Dpackaging=jar
